ca2acadcd402091d91d468aff5e1dbab892c61e5
omascorro <omar.mascorro@protonmail.com>
2025-08-28 14:56:21 -0600

    some lines where removed

--- Lessons/lesson2.js
+++ Lessons/lesson2.js
@@ -10,6 +10,8 @@ console.log(messageToPrint);
 var messageToPrint2 = `The Price for your ${itemName} is $${price}`;
 console.log(messageToPrint2);
  
+
+ 
 lñkasdlhkjadsfibjladsfbijldfswhilufdweshilufewhiufewqhiu
 ¿asdKHJLADSFLJGHDFSAGHILUFEWAHIUJFADS
 +adsfhdsafcbjlhcdsafhjglfsadhk
