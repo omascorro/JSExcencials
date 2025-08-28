091c83a53dd8c1372e2f30922da57e48c72233eb
omascorro <omar.mascorro@protonmail.com>
2025-08-28 12:52:48 -0600

    first commit

---
+++ Lessons/lesson1.js
@@ -0,0 +1,10 @@
+
+// Hellos world
+console.log("Hello, World!");
+
+// Variables and Data Types
+var firstName = "John"; // String
+var age = 30;
+let lastName = "Doe"; // String
+
+console.log("Name: " + firstName + " " + lastName + ", Age: " + age);
